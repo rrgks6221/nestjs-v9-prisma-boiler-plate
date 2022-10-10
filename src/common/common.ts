ee08066c5ac66452d08d7b69f384e08575ae15ea
LeeSeokHo <rrgks@naver.com>
2022-10-10 17:47:15 +0900

    Fix: stringBooleanTransform 카멜케이스 적용

--- src/common/common.ts
+++ src/common/common.ts
@@ -4,7 +4,7 @@ export const pageTransform = ({ value }) => {
   return Number(value) ? Number(value) - 1 : value;
 };
 
-export const StringBooleanTransform = ({ value }) => {
+export const stringBooleanTransform = ({ value }) => {
   if (value === BooleanString.True) return true;
   if (value === BooleanString.False) return false;
   return value;
